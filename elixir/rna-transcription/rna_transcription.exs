a9e100f611ff29568918e6a29ee6b341b1a9629e
Andrew P. MacDonald <macdonald.andrew.p@gmail.com>
2018-09-14 11:33:22 -0500

    rna transcription

---
+++ elixir/rna-transcription/rna_transcription.exs
@@ -0,0 +1,25 @@
+defmodule RNATranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map(dna, &transcribe/1)
+  end
+
+  @spec transcribe(char) :: char
+  defp transcribe(char) do
+    case char do
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
+      _ -> :invalid
+    end
+  end
+end
